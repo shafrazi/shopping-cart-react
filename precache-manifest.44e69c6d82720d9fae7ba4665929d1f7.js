a4090956bb586824bbbf716843a4701844fd53f6
shafrazi <shafrazi@live.com>
2020-10-16 00:41:00 +0530

    Updates

---
+++ precache-manifest.44e69c6d82720d9fae7ba4665929d1f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e752344e3ad8bb58a6f52992136edab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed20ce9c523a39dbe87",
+    "url": "/static/css/main.24100bc9.chunk.css"
+  },
+  {
+    "revision": "71e63514d65a7d4734f7",
+    "url": "/static/js/2.31ffe5da.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/static/js/2.31ffe5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed20ce9c523a39dbe87",
+    "url": "/static/js/main.e008ec1e.chunk.js"
+  },
+  {
+    "revision": "412abfcde7c316c2e803",
+    "url": "/static/js/runtime-main.57cd7e40.js"
+  },
+  {
+    "revision": "0c6dcec73deafc64ef6aee739b0a0590",
+    "url": "/static/media/discount.0c6dcec7.jpg"
+  },
+  {
+    "revision": "a7fceb3dc95e949cae230e874671ea77",
+    "url": "/static/media/hero.a7fceb3d.jpg"
+  }
+]);
